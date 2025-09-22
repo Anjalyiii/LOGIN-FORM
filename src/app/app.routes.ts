7242c67c60ab862bd0d5ba4c8a8df5710ee27888
Anjalyiii <anjali.chauhan@stupaanalytics.com>
2025-09-22 11:38:36 +0530

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 import { AboutComponent } from './components/about/about.component';
 import { CreateBinComponent } from './components/create-bin/create-bin.component';
 import { authGuard } from './auth.guard';
+import { UserFormComponent } from './components/user-form/user-form.component';
 
 export const routes: Routes = [
     {path:'login',component:LoginComponent},
@@ -15,6 +16,7 @@ export const routes: Routes = [
         import('./components/about/about.component').then(
           (mod) => mod.AboutComponent
         ),},
-    {path:'',redirectTo:"/login",pathMatch:'full'},        /*pathmatch should be full otherwise blank is in above two paths too*/
+    {path:'',redirectTo:"/login",pathMatch:'full'},   
+    {path: 'user-form', component: UserFormComponent},     /*pathmatch should be full otherwise blank is in above two paths too*/
     {path:'**',component:NotFoundComponent}               /*always write it in last otherwise it will show not found for every path including above all*/
 ];
